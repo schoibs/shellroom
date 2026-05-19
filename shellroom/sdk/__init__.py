6dcb8053ffe526b1825514dc01fb3761990d9d90
schoibs <choibingshen2001@gmail.com>
2026-05-20 00:42:25 +0800

    Implement shellroom sdk

---
+++ shellroom/sdk/__init__.py
@@ -0,0 +1,17 @@
+"""Client SDK for ShellRoom."""
+
+from shellroom.sdk.client import (
+    DEFAULT_SERVER_URL,
+    CreateRoomResult,
+    RoomSession,
+    ShellRoomClient,
+    ShellRoomError,
+)
+
+__all__ = [
+    "DEFAULT_SERVER_URL",
+    "CreateRoomResult",
+    "RoomSession",
+    "ShellRoomClient",
+    "ShellRoomError",
+]
